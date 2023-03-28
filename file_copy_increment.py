cd052ba070bb7c692d58ea6701bfbff44f034df7
Cimply <106759485+cimplylimited@users.noreply.github.com>
2023-03-28 16:35:45 -0400

    Update file_copy_increment.py
    Removing some of the commented out lines of code for clarity

--- file_copy_increment.py
+++ file_copy_increment.py
@@ -24,24 +24,3 @@ do_copy(dir_path, n_times)
 
 
 
-
-#print(os.getcwd())
-'''
-def do_copy(root, n_times):
-    assert os.path.exists(root)
-    print("This is the cwd:", os.getcwd())
-    os.chdir('/Users/johndoe/Git/LexDAO/LexDAO Membership Token/')
-    for file in os.listdir(os.curdir):
-        if not os.path.isfile(file):  # only change names if it's a file
-            continue
-        name, ext = os.path.splitext(file)
-        start_ind = int(0)
-        for n in range(1, n_times):
-            new_file = os.path.join(os.getcwd(), f"{start_ind+n}{ext}")
-            shutil.copy(os.path.join(os.getcwd(), file), new_file)
-
-            
-do_copy("/Users/johndoe/Git/LexDAO/LexDAO Membership Token/", 1)
-
-'''
-
