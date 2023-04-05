b31ce5e761235e58753e2393a45fc3cdecb2cc48
cimplylimited <cimplylimited@hotmail.com>
2023-04-05 00:30:13 -0400

    Token Incrementing Script inc. Token ID and File Name

--- file_copy_increment.py
+++ file_copy_increment.py
@@ -4,7 +4,7 @@
 import os
 import shutil
 
-dir_path ='/Users/johndoe/Git/LexDAO/LexDAO_Membership_Token' 
+dir_path ='/Users/johndoe/Git/LexDAO/LexDAO_Membership_Token/Image_File' 
 n_times = 5
 
 def do_copy(dir_path, n_times):
